1e6ba3ca5ff9fadc6e3449a31d45e6cd5f55b295
Kyle McConaughey <kylemcconaughey03@gmail.com>
2020-09-24 22:42:01 -0400

    basic styling copied from ajax notes app, will update to be different and prettier soon

--- notes/notesapp/settings.py
+++ notes/notesapp/settings.py
@@ -130,7 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = [BASE_DIR / 'static']
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 
 AUTH_USER_MODEL = 'users.User'
